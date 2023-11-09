2d0115e23e642125d3ab9fd2523474c7277826cb
Bouba <boubacar.konate@outlook.fr>
2023-11-09 12:34:52 +0100

    function clear

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 function ajouter(){
+    //récupération des valeurs contenues dans les inputs
     let produit = document.getElementById("prod").value
     let quantite = document.getElementById("qte").value
     let prix = document.getElementById("prix").value
     let pInfo = {"qte": quantite,                       //ce json correspondra à la value du localStorage & sessionStorage
                 "prix": prix};
-    console.log(pInfo);
-                        //key       //valeur
+    // console.log(pInfo);
+                        //key       //valeur       =>  localstorage
     localStorage.setItem(produit, JSON.stringify(pInfo))
     sessionStorage.setItem(prix,JSON.stringify(pInfo))
     sessionStorage.setItem(produit,JSON.stringify(pInfo))
 
+    //A l'ajout les inputs suivants seront vidés 
     // document.getElementById("prod").value = ""
     // document.getElementById("qte").value  = ""
     // document.getElementById("prix").value = ""
 
+    afficher()
+
 }
 
+
+//pour modifier, je clique sur le bouton modifier, ensuite j'écris mes nouvelles données, puis je valide avec le bouton ajouter 
 function modifier(){
     let produit = document.getElementById("prod").value
     let quantite = document.getElementById("qte").value
@@ -32,6 +38,7 @@ function modifier(){
     let prixModifie = JSON.parse(localStorage.getItem(prix))
     console.log(prixModifie);
 
+    afficher()
    
 }
 
@@ -46,7 +53,35 @@ function supprimer(){
     document.getElementById("qte").value = ""
     sessionStorage.removeItem(prix)
     document.getElementById("prix").value = ""
+
+    afficher()
 }
+
+function clear1(){                      //attention clear() est nom réservé = 
+    localStorage.clear()
+
+    afficher()
+}
+
+
+function afficher(){
+    let cle_recuperer = ""      //récupération de la key du localstorage
+    let paires = "<tr> <td>Produit</td> <td>Quantité</td> <td>Prix</td> </tr>"
+
+    for (let cpt = 0; cpt < localStorage.length; cpt++) {
+        cle_recuperer = localStorage.key(cpt)
+        // console.log(cle);
+
+        let data = JSON.parse(localStorage.getItem(cle_recuperer))
+        // console.log(data);
+
+        paires += "<tr> <td>"+cle_recuperer+"</td> <td>"+data.qte+"</td> <td>"+data.prix+"</td> </tr>"
+    } 
+
+    document.getElementById("display").innerHTML = paires
+}
+
+
 
 
 
