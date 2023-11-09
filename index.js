286860bf6ebfaf6a58bb87b4de822df144697cc2
Bouba <boubacar.konate@outlook.fr>
2023-11-09 10:44:46 +0100

    function ajouter /  modifier / supprimer

--- index.js
+++ index.js
@@ -10,8 +10,43 @@ function ajouter(){
     sessionStorage.setItem(prix,JSON.stringify(pInfo))
     sessionStorage.setItem(produit,JSON.stringify(pInfo))
 
-    document.getElementById("prod").value = ""
-    document.getElementById("qte").value  = ""
-    document.getElementById("prix").value = ""
+    // document.getElementById("prod").value = ""
+    // document.getElementById("qte").value  = ""
+    // document.getElementById("prix").value = ""
 
 }
+
+function modifier(){
+    let produit = document.getElementById("prod").value
+    let quantite = document.getElementById("qte").value
+    let prix = document.getElementById("prix").value
+    
+    // console.log(produit);
+    let produitModifie = JSON.parse(localStorage.getItem(produit))
+    console.log(typeof(produitModifie));
+    console.log(produitModifie);
+
+    let quantiteModifie = JSON.parse(localStorage.getItem(quantite))
+    console.log(quantiteModifie);
+
+    let prixModifie = JSON.parse(localStorage.getItem(prix))
+    console.log(prixModifie);
+
+   
+}
+
+function supprimer(){
+    let produit = document.getElementById("prod").value
+    let quantite = document.getElementById("qte").value
+    let prix = document.getElementById("prix").value
+
+    sessionStorage.removeItem(produit)
+    document.getElementById("produit").value = ""
+    sessionStorage.removeItem(quantite)
+    document.getElementById("qte").value = ""
+    sessionStorage.removeItem(prix)
+    document.getElementById("prix").value = ""
+}
+
+
+
